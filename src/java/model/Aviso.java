d08e159e849d0be00b0bc4a16a00cd3b0c4ac6d7
Jose Horacio de Oliveira Junior <80285876+josehoracio-oliveira@users.noreply.github.com>
2023-05-22 15:14:19 -0300

    Criação da Tabela Usuario e Aviso

--- src/java/model/Aviso.java
+++ src/java/model/Aviso.java
@@ -1,13 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Fatec
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Aviso {
-    
+
+    public static final String CLASS_NAME = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:sqlite:db_avisos.db";
+
+    public static Exception exception = null;
+
+    public static void createTableAviso() {
+        try {
+            Connection con = getConnection();
+            Statement stmt = con.createStatement();
+            stmt.execute("create table if not exists aviso (\n"
+                    + "id_aviso int auto_increment primary key,\n"
+                    + "nm_titulo varchar(100) not null,\n"
+                    + "ds_conteudo text,\n"
+                    + "id_usuario int not null,\n"
+                    + "constraint fk_usuario foreign key (id_usuario) references usuario (id_usuario))");
+            stmt.close();
+            con.close();
+        } catch (Exception ex) {
+            exception = ex;
+        }
+    }
+
+    public static Connection getConnection() throws Exception {
+        Class.forName(CLASS_NAME);
+        return DriverManager.getConnection(URL);
+    }
 }
--- src/java/model/Usuario.java
+++ src/java/model/Usuario.java
@@ -1,13 +1,34 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Fatec
- */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
 public class Usuario {
-    
+
+    public static final String CLASS_NAME = "org.sqlite.JDBC";
+    public static final String URL = "jdbc:sqlite:db_avisos.db";
+
+    public static Exception exception = null;
+
+    public static void createTableUsuario() {
+        try {
+            Connection con = getConnection();
+            Statement stmt = con.createStatement();
+            stmt.execute("create table if not exists usuario (\n"
+                    + "id_usuario int auto_increment primary key,\n"
+                    + "nome varchar(100) not null,\n"
+                    + "email varchar(50) not null unique))");
+            stmt.close();
+            con.close();
+        } catch (Exception ex) {
+            exception = ex;
+        }
+    }
+
+    public static Connection getConnection() throws Exception {
+        Class.forName(CLASS_NAME);
+        return DriverManager.getConnection(URL);
+    }
+
 }
