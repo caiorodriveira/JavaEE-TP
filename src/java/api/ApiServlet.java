091c2dfdc7aa5aaeb734e979b922d1823d281824
Pedro Bezerra <pedrohbs67@hotmail.com>
2023-05-28 10:02:08 -0300

    Remoção de Servlet Avisos

--- src/java/api/ApiServlet.java
+++ src/java/api/ApiServlet.java
@@ -54,31 +54,11 @@ public class ApiServlet extends HttpServlet {
         }
     }
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
        processRequest(request, response);
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
@@ -96,13 +76,6 @@ public class ApiServlet extends HttpServlet {
        processRequest(request, response);
     }
     
-    
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
     @Override
     public String getServletInfo() {
         return "Short description";
--- web/index.jsp
+++ web/index.jsp
@@ -1,26 +1,6 @@
 <%@page import="model.Usuario"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 
-<%
-    String loginErrorMessage = null;
-    if(request.getParameter("btn-registrar") != null){
-        String nome = request.getParameter("nome");
-        String email = request.getParameter("email");
-        int senha = (Integer) request.getParameter("senha").hashCode();
-        String senhaHash = Integer.toString(senha);
-        
-        try{
-               Usuario u = new Usuario();
-               u.addUsuario(nome, email, "USER", senhaHash);
-               
-            
-        } catch(Exception ex){
-            loginErrorMessage = ex.getMessage();
-        } finally {
-            response.sendRedirect(request.getContextPath()+"/login.jsp");
-        }
-    }
-%>
 <!DOCTYPE html>
 <html>
     <head>
@@ -29,17 +9,5 @@
     </head>
     <body>
         <h2>Registro</h2>
-        <fieldset>
-            <legend>Formulário de Registro</legend>
-            <form method="post">
-                <label for="nome">Nome</label>
-                <input type="text" name="nome" id="nome"/><br/>
-                <label for="email">Email</label>
-                <input type="email" name="email" id="email"/><br/>
-                <label for="senha">Senha</label>
-                <input type="password" name="senha" id="senha"/><br/><br/>
-                <input type="submit" name="btn-registrar" value="Entrar"/>
-            </form>
-        </fieldset>
     </body>
 </html>
