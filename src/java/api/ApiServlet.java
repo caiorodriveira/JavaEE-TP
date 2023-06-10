38cf6f3bef8509f0077d909d18b8c15b614a39fb
Pedro Bezerra <pedrohbs67@hotmail.com>
2023-06-10 09:13:25 -0300

    Tirando alguns ifs desnecessarios do servlet

--- src/java/api/ApiServlet.java
+++ src/java/api/ApiServlet.java
@@ -37,22 +37,22 @@ public class ApiServlet extends HttpServlet {
         }
         return new JSONObject(buffer.toString());
     }
-    
+
     protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("application/json;charset=UTF-8");
-        JSONObject file = new JSONObject(); 
-        try{
-           if(request.getRequestURI().endsWith("/api/session")){
-               processSession(file, request, response);
-           } else if(request.getRequestURI().endsWith("/api/usuarios")){
-               processUsuarios(file, request, response);
-           } else if(request.getRequestURI().endsWith("api/avisos")) {
-               processAvisos(file, request, response);
-           } else {
-               response.sendError(400, "URL invalida!");
-               file.put("error", "URL invalida");
-           }
-        } catch(Exception ex){
+        JSONObject file = new JSONObject();
+        try {
+            if (request.getRequestURI().endsWith("/api/session")) {
+                processSession(file, request, response);
+            } else if (request.getRequestURI().endsWith("/api/usuarios")) {
+                processUsuarios(file, request, response);
+            } else if (request.getRequestURI().endsWith("api/avisos")) {
+                processAvisos(file, request, response);
+            } else {
+                response.sendError(400, "URL invalida!");
+                file.put("error", "URL invalida");
+            }
+        } catch (Exception ex) {
             response.sendError(500, "Erro interno: " + ex.getLocalizedMessage());
             file.put("error", "Erro interno: " + ex.getLocalizedMessage());
         }
@@ -60,8 +60,9 @@ public class ApiServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       processRequest(request, response);
+        processRequest(request, response);
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         processRequest(request, response);
@@ -75,21 +76,21 @@ public class ApiServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-       processRequest(request, response);
+        processRequest(request, response);
     }
-    
+
     @Override
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
 
-    private void processSession(JSONObject file, HttpServletRequest request, HttpServletResponse response) throws Exception { 
-        if(request.getMethod().toLowerCase().equals("put")){
+    private void processSession(JSONObject file, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        if (request.getMethod().toLowerCase().equals("put")) {
             JSONObject body = getJSONBody(request.getReader());
             String email = body.getString("email");
             String senha = body.getString("senha");
             Usuario u = Usuario.getUsuario(email, senha);
-            if(u == null){
+            if (u == null) {
                 response.sendError(403, "Email ou Senha invalidos!");
             } else {
                 request.getSession().setAttribute("usuario", u);
@@ -98,17 +99,17 @@ public class ApiServlet extends HttpServlet {
                 file.put("email", u.getEmail());
                 file.put("role", u.getRole());
                 file.put("senha", u.getSenha());
-                
+
                 file.put("message", "Logado com sucesso!");
                 response.getWriter().print(file.toString());
             }
-        } else if(request.getMethod().toLowerCase().equals("delete")){
+        } else if (request.getMethod().toLowerCase().equals("delete")) {
             request.getSession().removeAttribute("usuario");
             file.put("message", "Deslogado com sucesso!");
             response.getWriter().print(file.toString());
-            
-        } else if(request.getMethod().toLowerCase().equals("get")){
-            if(request.getSession().getAttribute("usuario") == null){
+
+        } else if (request.getMethod().toLowerCase().equals("get")) {
+            if (request.getSession().getAttribute("usuario") == null) {
                 response.sendError(403, "Não existe sessão");
             } else {
                 Usuario u = (Usuario) request.getSession().getAttribute("usuario");
@@ -123,28 +124,29 @@ public class ApiServlet extends HttpServlet {
             response.sendError(405, "Method not allowed");
         }
     }
+
     private void processUsuarios(JSONObject file, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if(request.getMethod().toLowerCase().equals("get")){
-            if(!((Usuario)request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")){
-                response.sendError(401, "Erro! Apenas administradores podem visualizar usuarios!");
-            } else {
-                file.put("usuarios", new JSONArray(Usuario.getUsuarios()));
-                response.getWriter().print(file.toString());
-            }
-        } else if(request.getMethod().toLowerCase().equals("post")){
-            if(!((Usuario)request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")){
-                response.sendError(401, "Erro! Apenas administradores podem adicionar usuarios!");
-            } else {
-                JSONObject body = getJSONBody(request.getReader());
-                String nome = body.getString("nome");
-                String email = body.getString("email");
-                String senha = body.getString("senha");
-                Usuario.addUsuario(nome, email, "USER", senha, true);
- 
-                file.put("message", "Usuario cadastrado com sucesso!");
-                response.getWriter().print(file.toString());
-            }
-        } else if(request.getMethod().toLowerCase().equals("put")){
+        if (request.getSession().getAttribute("usuario") == null) {
+            response.sendError(401, "Não autorizado! Não existe sessão");
+        } else if (!((Usuario) request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")) {
+            response.sendError(401, "Erro! Apenas administradores podem manipular usuarios!");
+        } else if (request.getMethod().toLowerCase().equals("get")) {
+            
+            file.put("usuarios", new JSONArray(Usuario.getUsuarios()));
+            response.getWriter().print(file.toString());
+        } else if (request.getMethod().toLowerCase().equals("post")) {
+
+            JSONObject body = getJSONBody(request.getReader());
+            String nome = body.getString("nome");
+            String email = body.getString("email");
+            String senha = body.getString("senha");
+            Usuario.addUsuario(nome, email, "USER", senha, true);
+
+            file.put("message", "Usuario cadastrado com sucesso!");
+            response.getWriter().print(file.toString());
+
+        } else if (request.getMethod().toLowerCase().equals("put")) {
+
             JSONObject body = getJSONBody(request.getReader());
             Long id = Long.parseLong(request.getParameter("id"));
             String nome = body.getString("nome");
@@ -152,46 +154,43 @@ public class ApiServlet extends HttpServlet {
             String role = body.getString("role");
             String password = body.getString("senha");
             Usuario.updateUsuario(id, nome, email, password, role);
-            
+
             file.put("message", "Usuário atualizado com sucesso!");
             response.getWriter().print(file.toString());
-            
-        } else if(request.getMethod().toLowerCase().equals("delete")){
-            if(!((Usuario)request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")){
-                response.sendError(401, "Erro! Apenas administradores podem deletar usuarios!");
-            } else {
-                Long id = Long.parseLong(request.getParameter("id"));
-                Usuario.deleteUsuario(id);
-            
-                file.put("message", "Usuário deletado com sucesso!");
-                response.getWriter().print(file.toString());
-            }
+
+        } else if (request.getMethod().toLowerCase().equals("delete")) {
+
+            Long id = Long.parseLong(request.getParameter("id"));
+            Usuario.deleteUsuario(id);
+
+            file.put("message", "Usuário deletado com sucesso!");
+            response.getWriter().print(file.toString());
+
         }
     }
 
     private void processAvisos(JSONObject file, HttpServletRequest request, HttpServletResponse response) throws Exception {
-        if(request.getSession().getAttribute("usuario") == null){
+        if (request.getSession().getAttribute("usuario") == null) {
             response.sendError(401, "Erro! Usuário não está logado!");
-        }
-        else if(request.getMethod().toLowerCase().equals("get")){
+        } else if (request.getMethod().toLowerCase().equals("get")) {
             file.put("avisos", new JSONArray(Aviso.getAvisos()));
             response.getWriter().print(file.toString());
-        } else if(request.getMethod().toLowerCase().equals("post")){
-            if(!((Usuario)request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")){
+        } else if (request.getMethod().toLowerCase().equals("post")) {
+            if (!((Usuario) request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")) {
                 response.sendError(401, "Erro! Apenas administradores podem adicionar avisos!");
             } else {
                 JSONObject body = getJSONBody(request.getReader());
-                Long id = Long.parseLong(request.getParameter("idUsuario")); 
+                Long id = Long.parseLong(request.getParameter("idUsuario"));
                 String titulo = body.getString("titulo");
                 String conteudo = body.getString("conteudo");
                 String data = LocalDateTime.now().toString();
                 Aviso.addAviso(titulo, conteudo, data, id);
-                
+
                 file.put("message", "Aviso adicionado com sucesso!");
                 response.getWriter().print(file.toString());
             }
-        } else if(request.getMethod().toLowerCase().equals("put")){
-            if(!((Usuario)request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")){
+        } else if (request.getMethod().toLowerCase().equals("put")) {
+            if (!((Usuario) request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")) {
                 response.sendError(401, "Erro! Apenas administradores podem atualizar avisos!");
             } else {
                 JSONObject body = getJSONBody(request.getReader());
@@ -202,22 +201,21 @@ public class ApiServlet extends HttpServlet {
                 SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
                 String dataAtualString = formato.format(dataAtual);
                 Aviso.updateAviso(id, titulo, conteudo, dataAtualString);
-                
+
                 file.put("message", "Aviso atualizado com sucesso!");
                 response.getWriter().print(file.toString());
             }
-        } else if(request.getMethod().toLowerCase().equals("delete")){
-            if(!((Usuario)request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")){
+        } else if (request.getMethod().toLowerCase().equals("delete")) {
+            if (!((Usuario) request.getSession().getAttribute("usuario")).getRole().equals("ADMIN")) {
                 response.sendError(401, "Erro! Apenas administradores podem deletar avisos!");
             } else {
                 Long id = Long.parseLong(request.getParameter("idAviso"));
                 Aviso.removeAviso(id);
-                
+
                 file.put("message", "Aviso deletado com sucesso!");
                 response.getWriter().print(file.toString());
             }
         }
     }
-
 
 }
